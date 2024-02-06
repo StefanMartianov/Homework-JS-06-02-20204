12b9d13fbaa5642d217c686928fefc5294b42283
StefanMartianov <156689031+StefanMartianov@users.noreply.github.com>
2024-02-06 17:24:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// for (let i = 0; i <= 10;  i += 2) {
+//     console.log(i);
+
+// }
+
+// i = i + 2
+// i += 2
+
+// i = i + 1
+// i += 1
+// i++
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6];
+
+// for (let i = 55; i >= 20; i--){
+// console.log(i);
+// }
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6];
+//               0  1  2   3  4  5  6
+//массив - структура данных (array)
+// console.log(numbers[0]);
+
+// for (let index = 0; index < numbers.length; index++) {
+//     const element = numbers[index];
+//     console.log(element);
+// }
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6];
+// const numbers_squared = []
+// for (let index = 0; index < numbers.length; index++) {
+//   const element = numbers[index] **2;
+//   numbers_squared.push(element)
+// }
+// console.log(numbers_squared);
+// const lastElement = numbers_squared[6]
+// const lastElement = numbers_squared[numbers_squared.length - 1]
+
+// const user = {
+//   first_name: "Ivan",
+//   last_name: "Ivanov",
+//   age: 20,
+//   salary: 500,
+// };
+// console.log(user.age);
+// console.log(user["age"]);
+// let str = `Users name is ${user.first_name} ${user.last_name}. He is ${user.age} years old`
+// console.log(str);
